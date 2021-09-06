0a44a5517531b4d55c6582162d0bdd17a5c9d5bf
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-06 19:58:41 +0500

    Install Webpack and Jest

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'Hello Webpack!!!!';
+
+  return element;
+}
+
+document.body.appendChild(component());

fb0c8a998a13b0b1d5077e09ae70fdb7e84d09ba
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-06 20:28:15 +0100

    Update dist files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello Webpack!!!!';
-
-  return element;
-}
-
-document.body.appendChild(component());

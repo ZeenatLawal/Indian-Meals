ca0760fdbac32a08a8741ffd1e14f30610886d5a
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-07 12:43:48 +0500

    Add footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import './style.css';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello Webpack!!!!';
-
+  element.innerHTML = `Hello Webpack!`;
+  element.className = 'hello';
   return element;
 }
 
 document.body.appendChild(component());
+
+const footer = document.createElement('div');
+footer.className = 'footer';
+const footerText = document.createElement('p');
+footerText.innerHTML = 'Created By Microverse under CC lisence';
+footer.appendChild(footerText);
+document.body.appendChild(footer);

c7a11a843f01da0fd3662af15eacc8a05c9e949c
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-08 11:13:24 +0100

    Add eventlisteners to like icon and update likes on click

--- src/displayMeals.js
+++ src/displayMeals.js
@@ -1,24 +1,38 @@
 import commentPopup from './commentPopup.js';
+import { likes, postLikes } from './likesAPI.js';
 
 const main = document.getElementById('main');
 const header = document.getElementsByTagName('header');
 const footer = document.getElementsByTagName('footer');
 
+const likesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/7X6fjvwVL8rklkn24xVW/likes';
+
 export default (mealsDiv, allMeals) => {
   mealsDiv.innerHTML = '';
   allMeals.forEach((meal) => {
+    const spanId = meal.idMeal + 1;
     const card = `<img src="${meal.strMealThumb}" alt="Food">
     <div class="mealName flex">
       <p>${meal.strMeal}</p>
-      <i class="far fa-heart"></i>
+      <i class="far fa-heart" id="${meal.strMeal}"></i>
     </div>
-    <p id="likes">5 likes</p>
+    <span class="likes" id="${spanId}">0 likes</span>
     <button class="comment" id="${meal.idMeal}">Comments</button>`;
 
     const meals = document.createElement('div');
     meals.classList.add('mealCard');
     meals.innerHTML = card;
     mealsDiv.appendChild(meals);
+
+    const icon = document.getElementById(meal.strMeal);
+    const span = document.getElementById(spanId);
+
+    icon.addEventListener('click', () => {
+      postLikes(likesUrl, meal.idMeal).then(() => likes(likesUrl, span, meal.idMeal));
+    });
+
+    likes(likesUrl, span, meal.idMeal);
+
     const displayComment = document.getElementById(meal.idMeal);
     displayComment.addEventListener('click', () => {
       commentPopup(meal.idMeal);

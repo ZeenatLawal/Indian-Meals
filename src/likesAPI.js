9fff16cb4eabea590e4a82ca32d4c9ca9b1468ec
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-08 11:07:13 +0100

    Add function for showing likes on each meal card

--- src/likesAPI.js
+++ src/likesAPI.js
@@ -12,8 +12,15 @@ export const postLikes = async (url, mealId) => {
   return request;
 };
 
-export const getLikes = async (url) => {
+const getLikes = async (url) => {
   const request = await fetch(url);
   const result = await request.json();
   return result;
+};
+
+export const likes = (url, span, mealId) => {
+  getLikes(url).then((data) => {
+    data = data.filter((item) => item.item_id === mealId);
+    span.innerHTML = `${data[0].likes} likes`;
+  });
 };
